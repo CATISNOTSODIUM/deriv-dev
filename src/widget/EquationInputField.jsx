8707e676d7b8e8d1ab1d1975b0ad07b0a9d42829
unknown <thitiwatipst@icloud.com>
2024-04-26 19:36:41 +0700

    add mathjs

--- src/widget/EquationInputField.jsx
+++ src/widget/EquationInputField.jsx
@@ -1,19 +1,19 @@
 import MathJax from "react-mathjax2"; //for input field
-import React, { useState, useEffect } from "react";
-import { Form, Input } from "antd";
+import React, { useState, useEffect, Component,useRef } from "react";
+import { Form, Input, } from "antd";
 import "antd/dist/reset.css";
-
+import { derivative, symbolicEqual } from 'mathjs'
 //Sample equations
-const sampleEquation1 = "ln (x)";
+const sampleEquation1 = "";
 const sampleEquation2 = "c = a^2 + b^2";
 const sampleEquation3 =
     "f(a) = 1/(R_(si) + sum_(i=1)^n(s_n/lambda_n) + R_(se))";
-
+const displayProblem = "(sin(x))^2" 
 
 const EquationInputField = () => {
-    const [equation, setEquation] = useState(sampleEquation1);
-    const [form] = Form.useForm();
-
+    const inputRef = useRef(null);
+    var [equation, setEquation] = useState(sampleEquation1);
+    var [form] = Form.useForm();
     return (
         <div class=" w-screen h-auto min-h-40 flex justify-center align-center">
             <Form 
@@ -23,20 +23,47 @@ const EquationInputField = () => {
                 }}
                 className="bg-inherit caret-primaryTitle text-white text-3xl"
             >
-                <Form.Item name="equation" initialValue={equation} className="bg-inherit">
-                    <Input.TextArea autoFocus="autofocus" className="max-h-1 resize-none text-white border-none focus:border-none focus:bg-inherit hover:bg-inherit bg-primaryBackground placeholder-slate-300 font-semibold" placeholder="Type here"/>
-                </Form.Item>
-                <div className="relative">
+                <div className="h-20">
                     <MathJax.Context input="ascii">
-                        <div>
-                            <MathJax.Node>{equation}</MathJax.Node>
-                        </div>
+                        <MathJax.Node>{"d/dx (" + displayProblem + ")  =  "}</MathJax.Node>
+                    </MathJax.Context>
+                    <MathJax.Context input="ascii">
+                        <MathJax.Node>{equation}</MathJax.Node>
                     </MathJax.Context>
                 </div>
+                <Form.Item name="equation" initialValue={equation} className="bg-inherit">
+
+                <input ref={inputRef}  class="bg-inherit mt-10  caret-primaryTitle w-screen mx-36 font-mono text-2xl text-white text-center focus:outline-none" 
+                autoFocus="autofocus" placeholder='Input your equation here.'
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter"){
+                            //inputRef.current.value  //check the correctness
+                            if (symbolicEqual(derivative(polishQuestionExpression(displayProblem), 'x'), inputRef.current.value)){ //correct answer
+                                inputRef.current.value = "";
+                            } 
+                            
+                        }
+                    }}
+                ></input>
+                </Form.Item>
             </Form>
             
 
         </div>
 )};
+
+//submission system
+
+function addParenthesis(expression) {
+    let ans = expression.replace("ln", "log")
+    return ans;
+
+}
+function polishQuestionExpression(expression){
+    let ans = expression.replace("ln", "log")
+    //insert parenthesis over ln ... 
+    //find ln and find consecutive x -> not number -> cut
+
+    return ans;
+}
 export default EquationInputField;
-//<input class="bg-inherit caret-primaryTitle w-screen mx-36 text-4xl focus:outline-none" autoFocus="autofocus" placeholder='Input your equation here.'></input>
