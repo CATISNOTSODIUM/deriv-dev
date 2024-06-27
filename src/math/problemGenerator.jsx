48a419132ae8ab5d19b998e74a76c02d424bdcd3
Coffee Cat <thitiwatipst@gmail.com>
2024-06-27 15:17:31 +0700

    change problem type

--- src/math/problemGenerator.jsx
+++ src/math/problemGenerator.jsx
@@ -1,7 +1,7 @@
 import { polynomialGenerator, polynomialTrigGenerator } from "./polynomialGenerator";
 
 export function problemGenerator(numProblem){
-    var problemList = [...Array(numProblem).keys()].map(foo => polynomialTrigGenerator(Math.floor(Math.random() * 2) + 2));
+    var problemList = [...Array(numProblem).keys()].map(foo => polynomialGenerator(Math.floor(Math.random() * 2) + 2));
     
     return problemList;
 }
